63923bf50058b2058767d66af357def12d427dc8
Kiriakos Kanatsaris <kiriakoskanatsaris@gmail.com>
2021-11-14 23:24:57 +0200

    Basic multi-column support.

--- CCTweaked/autostock.lua
+++ CCTweaked/autostock.lua
@@ -1,6 +1,7 @@
 local settings = require "ae2.autostocksettings"
-local monitor = peripheral.find("monitor")
 local logged = { item={}, fluid={} }
+local displayProperties = 0
+local oldSizeX, oldSizeY = 0,0
 
 local Status = { OK = 1, CRAFTABLE = 2, UNCRAFTABLE = 3, CRAFTING = 4 }
 local StatusColours = {
@@ -9,6 +10,83 @@ local StatusColours = {
   [Status.UNCRAFTABLE] = colours.red,
   [Status.CRAFTING] = colours.lightBlue,
 }
+
+local monitor = peripheral.find("monitor")
+local width,height
+local resolutionScale = 0.5
+local resolutionTable = {
+        ["0.5"] = {{15,36,57,79,100,121,143,164},{10,24,38,52,67,81}},
+        ["1"]   = {{7,18,29,39,50,61,71,82},{5,12,19,26,33,40}},
+        ["1.5"] = {{5,12,19,26,33,40,48,55},{3,8,13,17,22,27}},
+        ["2"]   = {{4,9,14,20,25,30,36,41},{2,6,10,13,17,20}},
+        ["2.5"] = {{3,7,11,16,20,24,29,33},{2,5,8,10,13,16}},
+        ["3"]   = {{2,6,10,13,17,20,24,27},{2,4,6,9,11,13}},
+        ["3.5"] = {{2,5,8,11,14,17,20,23},{1,3,5,7,10,12}},
+        ["4"]   = {{2,5,7,10,13,15,18,21},{1,3,5,7,8,10}},
+        ["4.5"] = {{2,4,6,9,11,13,16,18},{1,3,4,6,7,9}},
+        ["5"]   = {{1,4,6,8,10,12,14,16},{1,2,4,5,7,8}},
+    }
+local screenSizeX, screenResX
+local screenSizeY, screenResY
+local scaledResX, scaledResY
+local columns
+local columnWidth
+local availableLines
+
+function NearestValue(table, number)
+    local smallestSoFar, smallestIndex
+    for i, y in ipairs(table) do
+        if not smallestSoFar or (math.abs(number-y) < smallestSoFar) then
+            smallestSoFar = math.abs(number-y)
+            smallestIndex = i
+        end
+    end
+    return smallestIndex, table[smallestIndex]
+end
+
+local function getMonSize(x,y,scale)
+   return resolutionTable[scale][1][x],resolutionTable[scale][2][y]
+end
+
+function initializeScreen()
+    monitor.setTextScale(1)
+    width,height = monitor.getSize()
+    monitor.setTextScale(resolutionScale)
+    screenSizeX, screenResX = NearestValue(resolutionTable["1"][1],width)
+    screenSizeY, screenResY = NearestValue(resolutionTable["1"][2],height)
+    scaledResX, scaledResY = getMonSize(screenSizeX,screenSizeY,tostring(resolutionScale))
+    availableLines = scaledResY-3
+
+    if screenSizeX <= 2 then
+        columns = 1
+    elseif screenSizeX == 3 then
+        columns = 2
+    elseif screenSizeX == 4 then
+        columns = 3
+    elseif screenSizeX == 5 then
+        columns = 3
+    elseif screenSizeX == 6 then
+        columns = 4
+    elseif screenSizeX == 7 then
+        columns = 4
+    elseif screenSizeX == 8 then
+        columns = 5
+    end
+
+    columnWidth = scaledResX / columns
+
+    if oldSizeX ~= screenSizeX or oldSizeY ~= screenSizeY then
+        displayUpdate = 1
+    end
+    if displayUpdate == 1 then
+        oldSizeX = screenSizeX
+        oldSizeY = screenSizeY
+        print("\nNew Monitor Size: "..screenSizeX.."x"..screenSizeY)
+        --print("New Standard Resolution: "..width.."x"..height)
+        print("New Year's Resolution: "..scaledResX.."x"..scaledResY)
+        displayUpdate = 0
+    end
+end
 
 local function inventory()
     local inv,err = ae2.getInventory()
@@ -95,54 +173,50 @@ local function restock(info)
     end
 end
 
+
+local function writeTitle(resX, resY, scale)
+    local programName = "AutoStock  v1.0.2"
+    local programTitle = "<- "..programName.." ->"
+    monitor.setTextScale(scale)
+    monitor.setCursorPos(1, 1)
+    monitor.write("/"..string.rep("-", resX-2).."\\")
+    monitor.setCursorPos(1, 2)
+    monitor.write("|")
+    monitor.setCursorPos((scaledResX/2)-(string.len(programTitle))/2+1, 2)
+    monitor.write(programTitle)
+    monitor.setCursorPos(scaledResX, 2)
+    monitor.write("|")
+    monitor.setCursorPos(1, 3)
+    monitor.write("\\"..string.rep("-", scaledResX-2).."/")
+end
+
 local function report(info, msg)
-    local width,height = monitor.getSize()
-    monitor.setTextScale(0.5)
+    initializeScreen()
+    local columnIndex = 0
     if msg then
         monitor.setBackgroundColour(colours.red)
         monitor.setTextColour(colours.white)
         monitor.clear()
-        monitor.setCursorPos((width - string.len(msg))/2+1, height/2+1)
+        writeTitle(scaledResX,scaledResY,resolutionScale)
+        monitor.setCursorPos((scaledResX - string.len(msg))/2+1, scaledResY/2+1)
         monitor.write(msg)
     else
         monitor.setBackgroundColour(colours.black)
         monitor.setTextColour(colours.white)
         monitor.clear()
-        monitor.setCursorPos(0, 1)
-        monitor.write(" /-------------------------------------------------------\\ ")
-        monitor.setCursorPos(0, 2)
-        monitor.write(" |                <- AutoStock  v1.0.2 ->                | ")
-        monitor.setCursorPos(0, 3)
-        monitor.write(" \\-------------------------------------------------------/ ")
 
-        local maxsummary = 0
-        for _,v in ipairs(info) do
-            maxsummary = math.max(maxsummary, string.len(v.summary))
-        end
+        writeTitle(scaledResX,scaledResY,resolutionScale)
 
-        local column = 30-2
-        local vt = 0
         for line,v in ipairs(info) do
-            vt=vt+1
-            if info[vt] ~= nil then
-                monitor.setCursorPos(2, line+3)
-                monitor.write(info[vt].name)
-                monitor.setTextColour(StatusColours[info[vt].status])
-                monitor.setCursorPos(column - string.len(info[vt].summary), line+3)
-                monitor.write(info[vt].summary)
-                monitor.setTextColour(colours.white)
-
-                vt=vt+1
-                if info[vt] ~= nil then
-                    monitor.setCursorPos(column + 2, line+3)
-                    monitor.write(info[vt].name)
-                    monitor.setTextColour(StatusColours[info[vt].status])
-                    monitor.setCursorPos(2 + column * 2 - string.len(info[vt].summary)-1, line+3)
-                    monitor.write(info[vt].summary)
-                    monitor.setTextColour(colours.white)
-                end
-            end
-            if line > height then break end
+            if line > (availableLines)*(columnIndex+1) and columnIndex < columns then
+                columnIndex = columnIndex + 1
+            elseif line > (availableLines)*(columnIndex+1) and columnIndex == columns then break end
+            monitor.setCursorPos(2 + (columnWidth*columnIndex), line + 3 - ((availableLines)*columnIndex))
+            monitor.write(v.name)
+            monitor.setTextColour(StatusColours[v.status])
+            monitor.setCursorPos((columnWidth*(columnIndex + 1))- 2 - string.len(tostring(x)), line + 3 -((availableLines)*columnIndex))
+            monitor.write(v.summary)
+            monitor.setTextColour(colours.white)
         end
     end
 end
@@ -153,7 +227,7 @@ function getSlim(n)
     elseif n >= 10^6 then
         return string.format("%.1fm", n / 10^6)
     elseif n >= 10^3 then
-        return string.format("%.1fk", n / 10^3)
+        return string.format("%.0fk", n / 10^3)
     else
         return tostring(n)
     end
@@ -185,6 +259,7 @@ end
 print("AutoStock running, press ENTER to halt")
 
 local run = true
+initializeScreen()
 while run do
     inv, msg = inventory()
     info = check(inv)
