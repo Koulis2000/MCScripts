59acc069ce92579f21df22950914b0d0c5ef38dd
Kiriakos Kanatsaris <kiriakoskanatsaris@gmail.com>
2021-11-21 23:45:05 +0200

    Changed from simple columns to windows. Added horizontal CPU list that shows available CPUs, their threads (coprocessors), storage and status. Bugged when only one CPU is found in the system.

--- CCTweaked/autostock.lua
+++ CCTweaked/autostock.lua
@@ -1,6 +1,8 @@
+sleep(1)
 local settings = require "ae2.autostocksettings"
 local logged = { item={}, fluid={} }
 local displayProperties = 0
+local CPUs = ae2.getCraftingCPUs()
 
 local Status = { OK = 1, CRAFTABLE = 2, UNCRAFTABLE = 3, CRAFTING = 4 }
 local StatusColours = {
@@ -10,7 +12,7 @@ local StatusColours = {
   [Status.CRAFTING] = colours.lightBlue,
 }
 
-local monitor = peripheral.wrap( "left" )
+local monitor = peripheral.find( "monitor" )
 local monitorResolutionXX, monitorResolutionXY
 local resolutionScale = 0.5
 local resolutionTable = {
@@ -33,6 +35,19 @@ local columnTable = {}
 local amountOfColumns
 local currentColumn
 
+local function dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
+
 function assert_arg (n,val,tp,verify,msg,lev)
     if type(val) ~= tp then
         error(("argument %d expected a '%s', got a '%s'"):format(n,tp,type(val)),lev or 2)
@@ -47,6 +62,16 @@ local function assert_string (n,s)
     assert_arg(n,s,'string')
 end
 
+function shorten(s,w)
+    local ellipsis = "..."
+    local n_ellipsis = utf8.len(ellipsis)
+    assert_string(1,s)
+    if utf8.len(s) > w then
+        return s:sub(1,w-n_ellipsis) .. ellipsis
+    end
+    return s
+end
+
 function nearestValue(table, number)
     local smallestSoFar, smallestIndex
     for i, y in ipairs(table) do
@@ -56,6 +81,18 @@ function nearestValue(table, number)
         end
     end
     return smallestIndex
+end
+
+function getSlim(n)
+    if n >= 10^9 then
+        return string.format("%.1fb", n / 10^9)
+    elseif n >= 10^6 then
+        return string.format("%.1fm", n / 10^6)
+    elseif n >= 10^3 then
+        return string.format("%.0fk", n / 10^3)
+    else
+        return tostring(n)
+    end
 end
 
 local function getMonitorResolution(mon)
@@ -74,11 +111,12 @@ local function getScaledMonitorResolution(mon, scale)
 end
 
 local function writeTitle(resX, resY, scale)
-    local programName = "AutoStock  v1.0.2"
+    local programName = "AutoStock  v1.0.4"
     local programTitle = "<- "..programName.." ->"
     monitor.setTextScale(scale)
+    monitor.setTextColour(colours.white)
     monitor.setCursorPos(1, 1)
-    monitor.write("/"..string.rep("-", resX-2).."\\")
+    monitor.write("/"..string.rep("¯", resX-2).."\\")
     monitor.setCursorPos(1, 2)
     monitor.write("|")
     monitor.setCursorPos((scaledMonitorResolutionX/2)-(string.len(programTitle))/2+1, 2)
@@ -86,13 +124,65 @@ local function writeTitle(resX, resY, scale)
     monitor.setCursorPos(scaledMonitorResolutionX, 2)
     monitor.write("|")
     monitor.setCursorPos(1, 3)
-    monitor.write("\\"..string.rep("-", scaledMonitorResolutionX-2).."/")
+    monitor.write("\\"..string.rep("_", scaledMonitorResolutionX-2).."/")
+end
+
+local function drawCPU(posX, posY, cpu, noCPU)
+    local CPUWindow = columnTable["columnWindow99"]
+    local coprocessors = cpu["coprocessors"].."T"
+    local storage = getSlim(cpu["storage"])
+    local descriptionPosition = (math.floor((11-(string.len(coprocessors..storage)))/2)+0.1)
+    local status = ae2.getCraftingCPUs()[noCPU]["busy"]
+    local statusText
+    local sizeX,sizeY = CPUWindow.getSize()
+    if status then
+        CPUWindow.setTextColour(colours.cyan)
+        statusText = "busy"
+    else
+        CPUWindow.setTextColour(colours.green)
+        statusText = "idle"
+    end
+    CPUWindow.setCursorPos(posX,posY)
+    CPUWindow.write(string.char(0x80)..string.char(0x80)..string.char(0x80)..string.char(0x94)..string.char(0x94)..string.char(0x94)..string.char(0x94)..string.char(0x94)..string.char(0x80)..string.char(0x80))
+    CPUWindow.setCursorPos(posX,posY+1)
+    CPUWindow.write(string.char(0x80)..string.char(0x80)..string.char(0x9c)..string.char(0x8d)..string.char(0x8d)..string.char(0x8d)..string.char(0x8d)..string.char(0x8d)..string.char(0x94)..string.char(0x80))
+    CPUWindow.setCursorPos(posX,posY+2)
+    CPUWindow.write(string.char(0x88)..string.char(0x86)..string.char(0x95).."CPU"..noCPU..string.char(0x80)..string.char(0x97)..string.char(0x8c))
+    CPUWindow.setCursorPos(posX,posY+3)
+    CPUWindow.write(string.char(0x88)..string.char(0x86)..string.char(0x95)..string.char(0x80)..string.char(0x80)..string.char(0x80)..string.char(0x80)..string.char(0x80)..string.char(0x97)..string.char(0x8c))
+    CPUWindow.setCursorPos(posX,posY+4)
+    CPUWindow.write(string.char(0x88)..string.char(0x86)..string.char(0x95)..string.char(0x80)..string.char(0x80)..string.char(0x80)..string.char(0x80)..string.char(0x80)..string.char(0x97)..string.char(0x8c))
+    CPUWindow.setCursorPos(posX,posY+5)
+    CPUWindow.write(string.char(0x88)..string.char(0x86)..string.char(0x95)..string.char(0x80)..statusText..string.char(0x97)..string.char(0x8c))
+    CPUWindow.setCursorPos(posX,posY+6)
+    CPUWindow.write(string.char(0x80)..string.char(0x80)..string.char(0x83)..string.char(0x97)..string.char(0x97)..string.char(0x97)..string.char(0x97)..string.char(0x97)..string.char(0x81)..string.char(0x80))
+    CPUWindow.setCursorPos(posX,posY+7)
+    CPUWindow.write(string.char(0x80)..string.char(0x80)..string.char(0x80)..string.char(0x81)..string.char(0x81)..string.char(0x81)..string.char(0x81)..string.char(0x81)..string.char(0x80)..string.char(0x80))
+    CPUWindow.setCursorPos(posX+descriptionPosition,posY+8)
+    CPUWindow.setTextColour(colours.magenta)
+    CPUWindow.write(coprocessors)
+    CPUWindow.setCursorPos(posX+descriptionPosition+string.len(coprocessors)+1,posY+8)
+    CPUWindow.setTextColour(colours.yellow)
+    CPUWindow.write(storage)
+end
+
+local function drawCPUs(xSize)
+    local cpuSize = 10
+    local cpuMargin = (xSize-cpuSize)/(#CPUs-1)
+    for i,v in ipairs(CPUs) do
+            drawCPU(1+(cpuMargin*(i-1)),1,CPUs[i],i)
+    end
+    local CPUWindow = columnTable["columnWindow99"]
+    CPUWindow.setCursorPos(1,cpuSize)
+    CPUWindow.setTextColour(colours.white)
+    CPUWindow.write(string.rep("-", scaledMonitorResolutionX))
 end
 
 function initializeScreen()
     local max_summary = 10
     local max_name = 20
     local requiredSpace = max_name+1+max_summary
+    local cpuColSize = 11
 
     monitor.setTextScale(resolutionScale)
 
@@ -105,17 +195,17 @@ function initializeScreen()
     scaledMonitorResolutionX, scaledMonitorResolutionY = getScaledMonitorResolution(monitor, resolutionScale)
     print("New Year's Resolution: "..scaledMonitorResolutionX.."x"..scaledMonitorResolutionY)
 
-    availableLines = scaledMonitorResolutionY-3
+    availableLines = scaledMonitorResolutionY-14
     amountOfColumns = math.floor((scaledMonitorResolutionX / requiredSpace)+0.1)
-    columnWidth = (scaledMonitorResolutionX / amountOfColumns)-2
+    columnWidth = ((scaledMonitorResolutionX) / amountOfColumns)
     print("It will comfortably fit "..amountOfColumns.." columns.".."\n")
 
     writeTitle(scaledMonitorResolutionX, scaledMonitorResolutionY, resolutionScale)
 
-    for key = 1, amountOfColumns, 1  do
-            columnTable["columnWindow"..tostring(key)] = window.create(monitor, columnWidth*(key-1)+2, 4, columnWidth, availableLines)
-            --columnTable["columnWindow"..tostring(key)].write("This is window "..key)
+    for key = 1, amountOfColumns,1  do
+            columnTable["columnWindow"..tostring(key)] = window.create(monitor, columnWidth*(key-1)+2, monitorResolutionY-availableLines, columnWidth, availableLines)
     end
+    columnTable["columnWindow99"] = window.create(monitor, 1, 4, scaledMonitorResolutionX, 10)
 end
 
 local function inventory()
@@ -203,58 +293,41 @@ local function restock(info)
     end
 end
 
-
-function shorten(s,w)
-    local ellipsis = "..."
-    local n_ellipsis = utf8.len(ellipsis)
-    assert_string(1,s)
-    if utf8.len(s) > w then
-        return s:sub(1,w-n_ellipsis) .. ellipsis
-    end
-    return s
-end
-
 local function report(info, msg)
     if msg then
         monitor.setBackgroundColour(colours.red)
         monitor.setTextColour(colours.white)
-        --monitor.clear()
         monitor.setCursorPos((scaledMonitorResolutionX - string.len(msg))/2+1, scaledMonitorResolutionY/2+1)
         monitor.write(msg)
     else
         monitor.setBackgroundColour(colours.black)
         monitor.setTextColour(colours.white)
-        --monitor.clear()
 
         for x = 1, amountOfColumns,1 do
             columnTable["columnWindow"..tostring(x)].clear()
         end
+        local lineIndex = 1
+        local column = 1
         for line,v in ipairs(info) do
-            local column = math.floor((line/availableLines)+1)
             local columnWindow = columnTable["columnWindow"..tostring(column)]
 
-            if line > (availableLines)*(column+1) and column == amountOfColumns then break end
-
             columnWindow.setTextColour(colours.white)
-            columnWindow.setCursorPos(1, line-(availableLines*(column-1)))
-            columnWindow.write(shorten(v.name,columnWidth-(utf8.len(v.summary)+1)))
+            columnWindow.setCursorPos(1, lineIndex)
+            columnWindow.write(string.format("%02d",line)..". ")
+            columnWindow.setCursorPos(5, lineIndex)
+            columnWindow.write(shorten(v.name,columnWidth-(utf8.len(v.summary)+6)))
             columnWindow.setTextColour(StatusColours[v.status])
-            columnWindow.setCursorPos((columnWidth+1) - string.len(tostring(v.summary)), line-(availableLines*(column-1)))
+            columnWindow.setCursorPos((columnWidth) - string.len(tostring(v.summary)), lineIndex)
             columnWindow.write(v.summary)
             columnWindow.setTextColour(colours.white)
+            lineIndex = lineIndex +1
+            if lineIndex > (availableLines) then
+                column = column + 1
+                lineIndex = 1
+            end
+            if lineIndex > (availableLines) and column == amountOfColumns then break end
         end
-    end
-end
-
-function getSlim(n)
-    if n >= 10^9 then
-        return string.format("%.1fb", n / 10^9)
-    elseif n >= 10^6 then
-        return string.format("%.1fm", n / 10^6)
-    elseif n >= 10^3 then
-        return string.format("%.0fk", n / 10^3)
-    else
-        return tostring(n)
+        drawCPUs(scaledMonitorResolutionX)
     end
 end
 
@@ -263,6 +336,10 @@ local function wait(seconds)
     while true do
         local event = {os.pullEventRaw()}
         if event[1] == "timer" and event[2] == t then return true end
+        if event[1] == "monitor_resize" then
+            print("\nMonitor size changed.")
+            initializeScreen()
+        end
         if event[1] == "terminate" then return false end
         if event[1] == "key" and event[2] == keys.enter then return false end
     end
@@ -284,13 +361,8 @@ end
 print("AutoStock running, press ENTER to halt")
 
 local run = true
-
+initializeScreen()
 while run do
-    monitorSizeX, monitorSizeY = getMonitorSize(monitor, resolutionScale)
-    if oldMonitorSizeX ~= monitorSizeX or oldMonitorSizeY ~= monitorSizeY then
-        print("\nMonitor size changed.")
-        initializeScreen()
-    end
     inv, msg = inventory()
     info = check(inv)
     restock(info)
